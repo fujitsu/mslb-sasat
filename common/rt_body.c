277d8e2b321aed9ae572a3d0aebfb9168c1d5251
Naoki Matsuhira <12995208+matsuhira@users.noreply.github.com>
2020-07-29 10:12:34 +0900

    Update rt_body.c

--- common/rt_body.c
+++ common/rt_body.c
@@ -2,7 +2,7 @@
  * file    rt_body.c 
  * brief   ルーティングソケットにより、nexthop IP, MACを検索する処理
  * note    COPYRIGHT FUJITSU LIMITED 2010
- *         FCT)Yagi
+ *         Yagi
  *
  * 改版履歴(出荷後記入)
  * 版数   日付    変更者    リリースノート
