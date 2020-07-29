c2eca8258b4862156013b0fed06426076b2b6b19
Naoki Matsuhira <12995208+matsuhira@users.noreply.github.com>
2020-07-29 10:16:00 +0900

    Update front.c

--- front/front.c
+++ front/front.c
@@ -2,7 +2,7 @@
  * file     front.c
  * brief    front translator処理 
  * note     COPYRIGHT FUJITSU LIMITED 2010
- *          FCT)Yagi
+ *          Yagi
  *
  * 改版履歴(出荷後記入)
  * 版数   日付    変更者    リリースノート
