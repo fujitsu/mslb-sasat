3efb1a67b6844aa57e4c67d9d296629892140683
Naoki Matsuhira <12995208+matsuhira@users.noreply.github.com>
2020-07-29 10:17:06 +0900

    Update front_properties.c

--- front/front_properties.c
+++ front/front_properties.c
@@ -2,7 +2,7 @@
  * file    front_properties.c
  * brief   プロパティファイル読み出し
  * note    COPYRIGHT FUJITSU LIMITED 2010
- *         FCT)Yagi
+ *         Yagi
  *
  * 版数   日付    変更者    リリースノート               
  * ---- -------- --------- --------------------------------------------------
